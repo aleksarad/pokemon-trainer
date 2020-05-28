395335d15b26f51e0cac659ccdb037d877c1cd21
aleksarad <alexa.radevich1@gmail.com>
2020-05-28 15:13:22 -0400

    refactoring/reorganizing code

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -16,6 +16,97 @@ class Interface
         self.train
     end
 
+    ##Functionality
+    #Menus
+
+    #select pokemon to train menu
+    def select_pokemon
+        self.chosen_pokemon = prompt.select("Choose a pokemon to train", Pokemon.list_pokemon_names)
+        self.pokemon_instance = Pokemon.find_pokemon_instance(self.chosen_pokemon)
+        puts "\n"
+    end
+
+    #type of training menu
+    def train 
+        Interface.clear
+        self.current_pokemon_stats
+        response = prompt.select("Train #{self.chosen_pokemon}?", ["Learn a new move", "Replace existing move", "Forget a move"])
+
+        if response == "Learn a new move"
+            self.learn_a_new_move
+            self.continue_training_or_exit
+        elsif response == "Replace existing move"
+            self.update_move
+            self.continue_training_or_exit
+        elsif response == "Forget a move"
+            self.delete_move
+            self.continue_training_or_exit
+        end
+    end
+
+    #continue training or exit menu
+    def continue_training_or_exit
+        answer = prompt.select("Continue training or exit?", ["Continue training #{self.chosen_pokemon}", "Train another pokemon", "Exit"])
+        if answer == "Exit"
+            self.exit_music_and_message
+        elsif answer == "Continue training #{self.chosen_pokemon}"
+            puts "\n"
+            self.train
+        elsif answer == "Train another pokemon"
+            Interface.clear
+            puts "\n"
+            self.select_pokemon
+            self.train
+        end
+    end
+
+    #Functionality helpers
+    def display_moves
+        answer = prompt.select("Choose a move", Move.list_move_names)
+        answer_arr = answer.split(' - ')
+        remove_brackets = answer_arr[0].delete('[]')
+    end
+
+    def learn_a_new_move
+        if self.pokemon_instance.moves.length < 4
+            chosen_move = self.display_moves
+            self.pokemon_instance.teach_move(chosen_move)
+            self.move_learned_animation_and_sound(chosen_move)
+        else
+            puts "\n Max moves learned! \n".colorize(:light_red)
+        end
+    end
+
+    def update_move 
+        if pokemon_instance.moves.length !=0
+            old_move = prompt.select("Select a move to replace", self.pokemon_instance.list_moves_names)
+            new_move = self.display_moves
+            self.pokemon_instance.update_move(old_move, new_move)
+            self.move_updated_animation_and_sound(old_move, new_move)
+        else 
+            puts "#{self.chosen_pokemon} doesn't know any moves to replace!".colorize(:red)
+        end
+    end
+
+    def delete_move 
+        if pokemon_instance.moves.length !=0
+            delete_move = prompt.select("Select a move to forget", self.pokemon_instance.list_moves_names)
+            self.pokemon_instance.delete_move_from_pokemon(delete_move)
+            self.move_deleted_animation_and_sound(delete_move)
+        else 
+            puts "#{self.chosen_pokemon} doesn't know any moves yet!".colorize(:red)
+        end
+    end
+
+    def current_pokemon_stats
+        puts " #{self.chosen_pokemon} ".colorize(:white).on_green.underline
+        puts "Type: #{self.pokemon_instance.poke_type}"
+        puts "Current Moves:"
+        puts self.pokemon_instance.list_moves_names
+        puts "\n"
+    end
+
+
     #CLI Helpers
     def start 
         prompt.select("These daycare pokemon need your help learning some moves. They're counting on you!",["Start"])
@@ -46,94 +137,34 @@ class Interface
         Ascii.logo
     end
 
-    ##Functionality
-    #Menus
-
-    #select pokemon to train menu
-    def select_pokemon
-        @chosen_pokemon = prompt.select("Choose a pokemon to train", Pokemon.list_pokemon_names)
-        @pokemon_instance = Pokemon.find_pokemon_instance(@chosen_pokemon)
-        puts "\n"
+    def move_learned_animation_and_sound(move_name)
+        Interface.learn_loading
+        puts "#{self.pokemon_instance.name} learned #{move_name}!".colorize(:light_cyan)
+        puts `afplay 'lib/music/move_learned.mov'`
+        puts "\n #{self.pokemon_instance.random_message}"
+        self.pokemon_instance.list_moves
     end
 
-    #type of training menu
-    def train 
+    def move_deleted_animation_and_sound(move_name)
+        Interface.delete_loading
+        print " #{self.pokemon_instance.name} forgot #{move_name}!".colorize(:light_cyan)
+        puts `afplay 'lib/music/SFX_Faint_No_HP_IMDOWN_rbysph.mp3'`
+        puts "\n #{self.pokemon_instance.random_message}"
+        self.pokemon_instance.list_moves
+    end
+
+    def move_updated_animation_and_sound(old_move, new_move)
+        Interface.learn_loading
+        puts "#{self.pokemon_instance.name} forgot #{old_move} and learned #{new_move}!".colorize(:light_cyan)
+        puts `afplay 'lib/music/move_learned.mov'`
+        puts "\n #{self.pokemon_instance.random_message}"
+        self.pokemon_instance.list_moves
+    end
+
+    def exit_music_and_message
         Interface.clear
-        self.current_pokemon_stats
-        response = prompt.select("Train #{@chosen_pokemon}?", ["Learn a new move", "Replace existing move", "Forget a move"])
-
-        if response == "Learn a new move"
-            self.learn_a_new_move
-            self.continue_training_or_exit
-        elsif response == "Replace existing move"
-            self.update_move
-            self.continue_training_or_exit
-        elsif response == "Forget a move"
-            self.delete_move
-            self.continue_training_or_exit
-        end
-        # Interface.clear
+        puts "\n The pokemon look so happy, please come back and train soon! \n".colorize(:green)
+        Ascii.pikachu
+        puts `afplay 'lib/music/pikachu.mp3'`
     end
-
-    #continue training or exit menu
-    def continue_training_or_exit
-        answer = prompt.select("Continue training or exit?", ["Continue training #{@chosen_pokemon}", "Train another pokemon", "Exit"])
-        if answer == "Exit"
-            Interface.clear
-            puts "\n The pokemon look so happy, please come back and train soon! \n".colorize(:green)
-            Ascii.pikachu
-        elsif answer == "Continue training #{@chosen_pokemon}"
-            puts "\n"
-            self.train
-        elsif answer == "Train another pokemon"
-            Interface.clear
-            puts "\n"
-            self.select_pokemon
-            self.train
-        end
-    end
-
-    #Functionality helpers
-    def display_moves
-        answer = prompt.select("Choose a move", Move.list_move_names)
-        answer_arr = answer.split(' - ')
-        remove_brackets = answer_arr[0].delete('[]')
-    end
-
-    def learn_a_new_move
-        if @pokemon_instance.moves.length < 4
-            chosen_move = self.display_moves
-            @pokemon_instance.teach_move(chosen_move)
-        else
-            puts "\n"
-            puts "Max moves learned!".colorize(:light_red)
-            puts "\n"
-        end
-    end
-
-    def update_move 
-        old_move = prompt.select("Select a move to replace", @pokemon_instance.list_moves_names)
-        new_move = self.display_moves
-        @pokemon_instance.update_move(old_move, new_move)
-    end
-
-    def delete_move 
-        delete_move = prompt.select("Select a move to forget", @pokemon_instance.list_moves_names)
-        @pokemon_instance.delete_move_from_pokemon(delete_move)
-        puts @pokemon_instance.list_moves
-    end
-
-    def current_pokemon_stats
-        puts " #{@chosen_pokemon} ".colorize(:white).on_green.underline
-        puts "Type: #{@pokemon_instance.poke_type}"
-        puts "Current Moves:"
-        puts @pokemon_instance.list_moves_names
-        puts "\n"
-    end
-
 end
-
-# Colorize color options:
-# [:black, :light_black, :red, :light_red, :green,
-#  :light_green, :yellow, :light_yellow, :blue, :light_blue,
-#   :magenta, :light_magenta, :cyan, :light_cyan, :white, :light_white, :default]
--- lib/models/pokemon.rb
+++ lib/models/pokemon.rb
@@ -2,23 +2,50 @@ class Pokemon < ActiveRecord::Base
     has_many :trainings
     has_many :moves, through: :trainings
 
-
-    #Create
+    #CRUD methods
     def teach_move(move_name)
         new_move = Move.find_by name: move_name
 
         if !self.list_moves_names.include?(move_name)
             self.moves << new_move
-            Interface.learn_loading
-            puts "#{self.name} learned #{move_name}!".colorize(:light_cyan)
-            puts `afplay 'lib/music/move_learned.mov'`
-            puts self.list_moves
         else
            puts "#{self.name} already knows #{move_name}!".colorize(:light_red)
         end
     end
 
-    #Read
+    def delete_move_from_pokemon(old_move)
+        old_move_instance = self.moves.find_by name: old_move
+
+        self.moves.delete(old_move_instance)
+    end
+
+    def update_move(old_move, new_move)
+        if !self.list_moves_names.include?(new_move)
+            delete_move_from_pokemon(old_move)
+            teach_move(new_move)
+        else 
+            puts "#{self.name} already knows #{new_move}!".colorize(:light_red) 
+        end
+
+        #figure out why this isn't working on the interface end
+        # if !self.list_moves_names.include?(new_move)
+        #     old_move_id = find_id(old_move)
+        #     new_move_id = find_id(new_move)
+        #     training_instance = self.trainings.find_by move_id: old_move_id
+        #     training_instance.update(move_id: new_move_id)
+        # else 
+        #     puts "#{self.name} already knows #{new_move}!".colorize(:light_red) 
+        # end
+        # self.list_moves
+    end
+
+    def find_id(move_name)
+        move_instance = Move.all.find_by name: move_name
+        move_instance.id
+    end
+
+
+    #move to interface?
     def list_moves
        names =  self.list_moves_names
        types = self.moves.collect{|move| move.move_type}
@@ -40,39 +67,40 @@ class Pokemon < ActiveRecord::Base
         ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
             #{names[3]}
                 #{types[3]} #{category[3]}
-        ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒        
-       ".colorize(:cyan)
+        ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒  
+
+       ".colorize(:light_cyan)
     end
 
+    #Helper Methods
     def list_moves_names
         self.moves.collect{|move| move.name}
     end
 
     def self.list_pokemon_names
-        self.all.collect{|pokemon| pokemon.name}
+        by_dex = self.all.sort_by {|pokemon| pokemon.dex_num}
+        by_dex.collect {|pokemon| pokemon.name}
     end
 
     def self.find_pokemon_instance(pokemon_name)
         self.all.find{|pokemon| pokemon.name == pokemon_name}
     end
 
-    #Delete
-    def delete_move_from_pokemon(old_move)
-        old_move_instance = self.moves.find_by name: old_move
-        Interface.delete_loading
-        print " #{self.name} forgot #{old_move}!".colorize(:light_cyan)
-        puts `afplay 'lib/music/SFX_Faint_No_HP_IMDOWN_rbysph.mp3'`
-        self.moves.delete(old_move_instance)
-    end
-
-    #Update
-    def update_move(old_move, new_move)
-        if !self.list_moves_names.include?(new_move)
-            delete_move_from_pokemon(old_move)
-            teach_move(new_move)
-        else 
-            puts "#{self.name} already knows #{new_move}!".colorize(:light_red) 
-        end
+    def random_message
+        ["#{self.name} looks sleepy.",
+        "#{self.name} wants to keep training!",
+        "#{self.name} yawned.", 
+        "#{self.name} is excited!",
+        "#{self.name} seems confused.",
+        "#{self.name} is having fun!",
+        "#{self.name} is surprised.", 
+        "#{self.name} is pumped!",
+        "#{self.name} fell asleep.",
+        "#{self.name} looked around.",
+        "#{self.name} seems to like you.",
+        "#{self.name} found a snack.",
+        "#{self.name} is jumping around!",
+        "#{self.name} made a noise!"].sample
     end
 end
 
